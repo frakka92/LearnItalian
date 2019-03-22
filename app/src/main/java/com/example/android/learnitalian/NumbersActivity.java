41a56b103a5a21085196f8bd30f2efc2665cef22
Francesca Serafini <serafini.francesca2@gmail.com>
2019-03-22 16:03:26 +0100

    icon

--- app/src/main/java/com/example/android/learnitalian/NumbersActivity.java
+++ app/src/main/java/com/example/android/learnitalian/NumbersActivity.java
@@ -1,23 +1,9 @@
-/*
- * Copyright (C) 2016 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.android.learnitalian;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -28,12 +14,7 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        // Create an array of words
-        String[] numbers = {"one","two","three","four","five","six","seven","eight","nine","ten"};
-
-
-        Log.v("NumbersActivity", numbers[0]);
-        ArrayList<String> myArray = new ArrayList<String>();
+        ArrayList<String> myArray = new ArrayList<>();
 
         myArray.add("zero");
         myArray.add("one");
@@ -46,10 +27,12 @@ public class NumbersActivity extends AppCompatActivity {
         myArray.add("eight");
         myArray.add("ten");
 
-        Log.v("NumbersActivity", myArray.get(6));
+        LinearLayout rootView = findViewById(R.id.linear_root);
 
-
-
-
+        for(int i=0; i < myArray.size();i++) {
+            TextView txtNumber = new TextView(this);
+            txtNumber.setText(myArray.get(i));
+            rootView.addView(txtNumber);
+          }
     }
 }
